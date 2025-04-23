d7cd4654814097b018dc6880289bdbacb277e3e5
Vinesh bhardwaj <vineshkumar5772@gmail.com>
2025-04-23 05:46:54 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Quiz from './COmponents/Quiz/Quiz'
+
+const App = () => {
+  return (
+    <>
+    <Quiz />
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+    question: "Which device is required for the internet connection?",
+    option1: "Moderm",
+    option2: "Router",
+    option3: "LAN Cable",
+    option4: "Drive",
+    ans: 1,
+},
+{
+    question: "Which Continent has the highest number of countries?",
+    option1: "Asia",
+    option2: "Europe",
+    option3: "North America",
+    option4: "Africa",
+    ans: 4,
+
+},
+{
+    question: "Junk e-mail is also called?",
+    option1: "spam",
+    option2: "Fake",
+    option3: "Archived",
+    option4: "Bin",
+    ans: 1,
+},
+{
+    question: "A computer cannot BOOT if it does not have the?",
+    option1: "Application Software",
+    option2: "Internet",
+    option3: "Operating System",
+    option4: "Mouse",
+    ans: 3,
+},
+{
+    question: "First page of website is termed as?",
+    option1: "Index Page",
+    option2: "Home Page",
+    option3: "Sitmap",
+    option4: "Pen drive",
+    ans: 2,
+},];
